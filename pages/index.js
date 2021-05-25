941207b48ed1338cbc741c2589bb4486bbb1313d
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2021-05-25 17:39:42 -0300

    'primeiro-teste'

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,5 @@
 import Link from 'next/link'
 
-function Clicar(){
-    var inicio = Number(document.getElementById('num1').value)
-    var fim = Number(document.getElementById('num2').value)
-    var cont = Number(document.getElementById('cont').value)
-    var res = document.getElementById('res')
-    if(cont == 0){
-        res.innerHTML = `[ERRO] COLOQUE O VALOR DO CONTADOR`
-    }else{
-        res.innerHTML = ``
-        if(inicio<fim){
-            for(inicio; inicio<=fim; inicio+=cont){
-                if(inicio != fim){
-                    res.innerHTML += String(inicio) + `\u{1F449}`
-                }else{
-                    res.innerHTML += String(inicio) + `\u{270B}`
-                }
-            }
-        }else{
-            for(inicio; inicio>=fim; inicio-=cont){
-                if(inicio != fim){
-                    res.innerHTML += String(inicio) + `\u{1F449}`
-                }else{
-                    res.innerHTML += String(inicio) + `\u{270B}`
-                }
-            }
-        }
-    }
-}
-
 function Home() {
     return (<div>
         <header>
@@ -45,7 +16,7 @@ function Home() {
                 Contagem: <input type="number" name="cont" id="cont"></input>
             </div>
             <div id='btn'>
-                <input id="button" type="button" value="Contar" onclick={Clicar()}></input>
+                <input id="button" type="button" value="Contar"></input>
             </div>
             <div id="res">
 
